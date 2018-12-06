4142f76a3d39b76487dde60e97efa9dc4f4879e8
Coll <31804171+coll2000@users.noreply.github.com>
2018-12-06 11:50:47 +0000

    Create Trundle.java

---
+++ Trundle.java
@@ -0,0 +1,28 @@
+import lejos.robotics.subsumption.Behavior;
+
+public class Trundle implements Behavior {
+	
+  //skeleton for trundle class - will update when not dead
+	
+	public Trundle() {
+		
+	}
+
+	@Override
+	public boolean takeControl() {
+		return false;
+	}
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void suppress() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
